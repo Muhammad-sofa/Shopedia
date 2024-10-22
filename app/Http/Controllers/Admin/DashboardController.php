a0c5e751f57c52c03a05bf763e938ea444de23a3
Sofa <muhammadsofa52@yahoo.com>
2024-10-22 14:51:48 +0700

    Create Static Page

--- app/Http/Controllers/Admin/DashboardController.php
+++ app/Http/Controllers/Admin/DashboardController.php
@@ -2,12 +2,22 @@
 
 namespace App\Http\Controllers\Admin;
 
-use App\Http\Controllers\Controller;
+use App\Models\User;
+use App\Models\Transaction;
 use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
 
 class DashboardController extends Controller
 {
-    public function index() {
-        return view('pages.admin.dashboard');
+    public function index() 
+    {
+        $customer = User::count();
+        $revenue = Transaction::where('transaction_status', 'SUCCESS')->sum('total_price');
+        $transaction = Transaction::count();
+        return view( 'pages.admin.dashboard', [
+            'customer' => $customer,
+            'revenue' => $revenue,
+            'transaction' => $transaction
+        ]);
     }
 }
--- resources/views/pages/admin/dashboard.blade.php
+++ resources/views/pages/admin/dashboard.blade.php
@@ -25,7 +25,7 @@ Admin Dashboard
                                         Customer
                                    </div>
                                    <div class="dashboard-card-subtitle">
-                                        Rp. 4.000.000
+                                        {{ $customer }}
                                    </div>
                               </div>
                          </div>
@@ -37,7 +37,7 @@ Admin Dashboard
                                         Revenue
                                    </div>
                                    <div class="dashboard-card-subtitle">
-                                        Rp. 5.000.000
+                                        Rp. {{ $revenue }}
                                    </div>
                               </div>
                          </div>
@@ -49,81 +49,10 @@ Admin Dashboard
                                         Transaction
                                    </div>
                                    <div class="dashboard-card-subtitle">
-                                        Rp. 5.000.000
+                                        Rp. {{ $transaction }}
                                    </div>
                               </div>
                          </div>
-                    </div>
-               </div>
-
-               <div class="row mt-3">
-                    <div class="col-12 mt-2">
-                         <h5 class="mb-3">
-                              Recent Transactions
-                         </h5>
-                         <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                              <div class="card-body">
-                                   <div class="row">
-                                        <div class="col-md-1">
-                                             <img src="/images/dashboard-icon-product-1.png" alt="">
-                                        </div>
-                                        <div class="col-md-4">
-                                             Shirup Marzzan
-                                        </div>
-                                        <div class="col-md-3">
-                                             Sofa
-                                        </div>
-                                        <div class="col-md-3">
-                                             09 Oktober, 2024
-                                        </div>
-                                        <div class="col-md-1 d-none d-md-block">
-                                             <img src="/images/dashboard-arrow-right.svg" alt="">
-                                        </div>
-                                   </div>
-                              </div>
-                         </a>
-                         <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                              <div class="card-body">
-                                   <div class="row">
-                                        <div class="col-md-1">
-                                             <img src="/images/dashboard-icon-product-2.png" alt="">
-                                        </div>
-                                        <div class="col-md-4">
-                                             LeBrone X
-                                        </div>
-                                        <div class="col-md-3">
-                                             Affan
-                                        </div>
-                                        <div class="col-md-3">
-                                             09 Oktober, 2024
-                                        </div>
-                                        <div class="col-md-1 d-none d-md-block">
-                                             <img src="/images/dashboard-arrow-right.svg" alt="">
-                                        </div>
-                                   </div>
-                              </div>
-                         </a>
-                         <a href="/dashboard-transactions-details.html" class="card card-list d-block">
-                              <div class="card-body">
-                                   <div class="row">
-                                        <div class="col-md-1">
-                                             <img src="/images/dashboard-icon-product-3.png" alt="">
-                                        </div>
-                                        <div class="col-md-4">
-                                             Soffa Lembutte
-                                        </div>
-                                        <div class="col-md-3">
-                                             Adit
-                                        </div>
-                                        <div class="col-md-3">
-                                             09 Oktober, 2024
-                                        </div>
-                                        <div class="col-md-1 d-none d-md-block">
-                                             <img src="/images/dashboard-arrow-right.svg" alt="">
-                                        </div>
-                                   </div>
-                              </div>
-                         </a>
                     </div>
                </div>
           </div>
