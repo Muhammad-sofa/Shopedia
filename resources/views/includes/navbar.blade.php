189aaf38641b5443de82df7c4e7b6f5c0e1867ee
Sofa <muhammadsofa52@yahoo.com>
2024-10-29 11:15:26 +0700

    Integration Login Update Navigation Section

--- resources/views/includes/navbar.blade.php
+++ resources/views/includes/navbar.blade.php
@@ -17,13 +17,54 @@
                     <li class="nav-item">
                          <a href="#" class="nav-link">Rewards</a>
                     </li>
+                    @guest
                     <li class="nav-item">
-                         <a href="/register.html" class="nav-link">Sign Up</a>
+                         <a href="{{ route('register') }}" class="nav-link">Sign Up</a>
                     </li>
                     <li class="nav-item">
-                         <a href="/login.html" class="btn btn-success nav-link px-4 text-white">Sign In</a>
+                         <a href="{{ route('login') }}" class="btn btn-success nav-link px-4 text-white">Sign In</a>
                     </li>
+                    @endguest
                </ul>
+
+               @auth
+                    <!--Desktop Menu-->
+                    <ul class="navbar-nav d-none d-lg-flex">
+                         <li class="nav-item dropdown">
+                              <a href="#" class="nav-link" id="navbarDropdown" role="button" data-toggle="dropdown">
+                                   <img src="/images/icon-user.png" alt="" class="rounded-circle mr-2 profile-picture">
+                                   Hi, {{ Auth::user()->name }}
+                              </a>
+                              <div class="dropdown-menu">
+                                   <a href="{{ route('dashboard') }}" class="dropdown-item">Dashboard</a>
+                                   <a href="{{ route('dashboard-settings-account') }}" class="dropdown-item">Settings</a>
+                                   <div class="dropdown-divider">
+                                        <a href="{{ route('logout') }}"
+                                        onclick="event.preventDefault(); document.getElementById('logout-form').submit();" class="dropdown-item">Logout</a>
+                                   </div>
+                              </div>
+                         </li>
+                         <li class="nav-item">
+                              <a href="#" class="nav-link d-inline-block mt-2">
+                                   <img src="/images/icon-cart-empty.svg" alt="">
+                              </a>
+                         </li>
+                    </ul>
+
+                    <!--Mobile Menu-->
+                    <ul class="navbar-nav d-block d-lg-none">
+                         <li class="nav-item">
+                              <a href="#" class="nav-link">
+                                   Hi, Sofa
+                              </a>
+                         </li>
+                         <li class="nav-item">
+                              <a href="" class="nav-link d-inline-block">
+                                   Card
+                              </a>
+                         </li>
+                    </ul>
+               @endauth
           </div>
      </div>
 </nav>
