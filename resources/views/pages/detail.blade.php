8583b428e04e978af3412befb46b23ae335bc25f
Sofa <muhammadsofa52@yahoo.com>
2024-10-30 05:56:23 +0700

    Add Function to Cart

--- resources/views/pages/detail.blade.php
+++ resources/views/pages/detail.blade.php
@@ -60,7 +60,7 @@ Store Detail Page
                          </div>
                          <div class="col-lg-2" data-aos="zoom-in">
                               @auth('admin')
-                                   <form action="#" method="POST" enctype="multipart/form-data">
+                                   <form action="{{ route('detail-add', $product->id) }}" method="POST" enctype="multipart/form-data">
                                         @csrf
                                         <button type="submit" class="btn btn-success px-4 text-white btn-block mb-3">Add to
                                              Cart
