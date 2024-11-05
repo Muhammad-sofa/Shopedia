1cd02c926818bb63ffc67e095831a9ba64099373
Sofa <muhammadsofa52@yahoo.com>
2024-11-05 14:47:53 +0700

    CRUD Transaction

--- routes/web.php
+++ routes/web.php
@@ -52,6 +52,7 @@ Route::prefix('admin')
      Route::resource('user', 'UserController');
      Route::resource('product', 'ProductController');
      Route::resource('product-gallery', 'ProductGalleryController');
+     Route::resource('transaction', 'TransactionController');
 });
 
 Auth::routes();
